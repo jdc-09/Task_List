db46946e7ef8679ce947bd3ef1e25849e88f3cc3
Juan David Castillo Caraballo <juancaraballo508@gmail.com>
2025-05-06 20:11:39 -0500

    Se gregaron las funciones de eliminar y marcar como completada

--- controller.py
+++ controller.py
@@ -3,6 +3,23 @@ from view import *
 
 
 
+while True:
+    opcion = obtener_seleccion()
 
-opcion = obtener_seleccion()
 
+    if opcion==1 : 
+        nombre_tarea=obtener_tarea()
+        agregar_tarea(nombre_tarea)
+        
+    elif opcion==2:
+        todas_tareas=listar_tareas()
+        mostrar_tareas(todas_tareas)
+
+    elif opcion==3:
+        indice=obtener_indice()
+        marcar_completada(indice)
+        
+
+    else: 
+        print("Bye")
+        break
