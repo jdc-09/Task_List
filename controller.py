71544536ff23630b91ba9becbbd687880d23942a
Juan David Castillo Caraballo <juancaraballo508@gmail.com>
2025-05-06 20:56:28 -0500

    Se agregó controlador y main

--- controller.py
+++ controller.py
@@ -2,29 +2,29 @@ import model
 import view
 
 
+def iniciar():
+    while True:
+        opcion = view.obtener_seleccion()
 
-while True:
-    opcion = view.obtener_seleccion()
 
+        if opcion==1 : 
+            nombre_tarea=view.obtener_tarea()
+            model.agregar_tarea(nombre_tarea)
+            
+        elif opcion==2:
+            todas_tareas=view.listar_tareas()
+            model.mostrar_tareas(todas_tareas)
 
-    if opcion==1 : 
-        nombre_tarea=view.obtener_tarea()
-        model.agregar_tarea(nombre_tarea)
+        elif opcion==3:
+            indice=view.obtener_indice()
+            model.marcar_completada(indice)
+
+        elif opcion==4:
+            indice = view.obtener_indice()
+            model.eliminar_tarea(indice)
         
-    elif opcion==2:
-        todas_tareas=view.listar_tareas()
-        model.mostrar_tareas(todas_tareas)
+            
 
-    elif opcion==3:
-        indice=view.obtener_indice()
-        model.marcar_completada(indice)
-
-    elif opcion==4:
-        indice = view.obtener_indice()
-        model.eliminar_tarea(indice)
-    
-        
-
-    else: 
-        print("Bye")
-        break
+        else: 
+            print("Bye")
+            break
